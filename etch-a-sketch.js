a832dd16decfdde8c464ab876bc878bdc50e6012
Fabio Ferreirinha <fabio.fereirinha@gmail.com>
2022-05-10 19:11:55 +0100

    Add cell coloring
    Cell color now gets changed to black when mouse leaves cell

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -2,8 +2,13 @@ const container = document.getElementById('container');
 // Creating 256 divs
 for(i = 0; i < 256; i++) {
   const cell = document.createElement('div');
-  cell.id =   'cell';
+  cell.className = 'cell';
 
-  container.appendChild(cell);
+  container.appendChild(cell); 
+
+  cell.addEventListener("mouseleave", () => cell.id ='colored');
 }
 
+function fillCell(target) {
+  target.id = 'colored';
+}
