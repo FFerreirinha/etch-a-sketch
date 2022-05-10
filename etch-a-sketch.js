62b3b1961d05bee054a4f63c7fb2a00aa8a5aa99
Fabio Ferreirinha <fabio.fereirinha@gmail.com>
2022-05-10 20:07:38 +0100

    Add resize and clear buttons
    Resize buttons to change grid size, clear button to make grid white (not functioning yet)

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,47 @@
 const container = document.getElementById('container');
-// Creating 256 divs
-for(i = 0; i < 256; i++) {
-  const cell = document.createElement('div');
-  cell.className = 'cell';
+const resizeButton = document.getElementById('resizebutton');
+const clearButton = document.getElementById('clearbutton');
+const cells = document.getElementsByClassName('class');
 
-  container.appendChild(cell); 
+function drawGrid(size) { 
+  for(i = 0; i < size; i++) {
+    const cell = document.createElement('div');
+    cell.className = 'cell';
 
-  cell.addEventListener("mouseleave", () => cell.id ='colored');
+    container.appendChild(cell); 
+
+    cell.addEventListener("mouseleave", () => cell.id ='colored');
+  } 
 }
 
 function fillCell(target) {
   target.id = 'colored';
 }
+
+function gridResize() {
+  let desiredSize = 0;
+
+  do {
+    desiredSize = parseInt(prompt("Select desired size up to 100x100. Input like: '20' for 20x20 grid"));
+  } while (desiredSize > 100);
+
+  let htmlStyles = window.getComputedStyle(document.querySelector("html"));
+  let gridSize = parseInt(htmlStyles.getPropertyValue("--gridSize"));
+  gridSize = document.documentElement.style.setProperty("--gridSize", desiredSize);
+
+  desiredSize = desiredSize * desiredSize;
+  container.textContent = "";
+
+
+  drawGrid(desiredSize);
+}
+
+function clearGrid() {
+  cells.id = "";
+}
+
+
+drawGrid(256); // Initial 16x16 grid
+
+resizeButton.addEventListener("click", () => gridResize());
+clearButton.addEventListener("click", () => clearGrid());
